b8606ef0bbf6720c80aeadf6dd2bf957ad533651
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2025-02-25 02:02:01 +0700

    chore: remove seaorm

--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -3,7 +3,6 @@ pub mod axum;
 pub mod jsonwebtoken;
 pub mod lettre;
 pub mod redis;
-pub mod seaorm;
 pub mod surrealdb;
 
 pub use argon::*;
@@ -11,5 +10,4 @@ pub use axum::*;
 pub use jsonwebtoken::*;
 pub use lettre::*;
 pub use redis::*;
-pub use seaorm::*;
 pub use surrealdb::*;
